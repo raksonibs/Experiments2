9af17017d70787cec600b8cfe9115fdfd3ce2f9c
Oskar Niburski <oskarniburski@gmail.com>
2016-01-07 15:27:24 -0500

    Book counter

--- 2031/scripts/ruby.rb
+++ 2031/scripts/ruby.rb
@@ -1,4 +1,4 @@
 1. find . -type f | wc -l
 2. find . -type f | grep 'HW' | wc -l
 3. find . -type f | grep -i 'hw' | wc -l
-4. echo 'bookisabook' >> test.txt | cat test.txt | grep -c 'book'
+4. echo 'bookisbook' > test.txt | cat test.txt | grep -o 'book' | wc -l
