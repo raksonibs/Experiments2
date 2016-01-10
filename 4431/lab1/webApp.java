e0b6286984f1a1a2d6b4887234957a928de88b60
Oskar Niburski <oskarniburski@gmail.com>
2016-01-09 19:15:03 -0500

    Redirect

--- 4431/lab1/webApp.java
+++ 4431/lab1/webApp.java
@@ -17,7 +17,27 @@ public class webApp extends HttpServlet {
     System.out.println(req.getMethod());
     System.out.println(req.getQueryString());
     System.out.println(req.getParameter("name"));
-    resp.getWriter().println("Testing Tomcat");
+    System.out.println(req.getContextPath());
+    System.out.println(req.getRequestURL());
+    System.out.println(req.getServletPath());
+    
+    System.out.println(req.getPathTranslated());
+    System.out.println(req.getPathInfo());
+    
+    System.out.println(this.getServletContext().getContextPath());
+  
+    
+    System.out.println("Approaching Redirect");
+    
+    if (req.getPathInfo() != null && req.getPathInfo().equals("/YorkBank")) {
+      System.out.println("Redirecting");
+      String  url = this.getServletContext().getContextPath() + "/Start";
+      resp.sendRedirect(url);
+    } else {
+      System.out.println("Not Redirecting");
+      resp.getWriter().println("Testing Tomcat");
+    }
+  
   }
   
 }
