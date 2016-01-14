d6fe136500d178c5d715e33f0a4d24e5d3bcfb06
Oskar Niburski <oskarniburski@gmail.com>
2016-01-13 23:23:46 -0500

    Splits the string, how to store now?

--- 2031/scripts/lab2.c
+++ 2031/scripts/lab2.c
@@ -2,44 +2,36 @@
 #include <stdlib.h>
 #include <string.h>
 
-static int prompt_and_read(const char *prompt, float *value)
-{
-    printf("%s", prompt);
-    if (scanf("%f", value) != 1)
-        return -1;
-    return 0;
-}
-
 main() {
-  float value;
-  float sum = 0.0;
-  int   num = 0;
+ 
+ char buffer[50];
+ FILE *stream;
+ stream = fopen("input_1.txt" , "rb");
+ char * pch;
+ while (fgets(buffer, sizeof buffer, stream) != NULL)
+ {
+   // process buffer
+  printf(buffer);
+  pch = strtok (buffer," ,.-");
+  while (pch != NULL)
+  {
+    printf ("%s\n",pch);
+    pch = strtok (NULL, " ,.-");
+  }
+  printf("File not done\n");
+ }
+ if (feof(stream))
+ {
+   // hit end of file
+  printf("File done\n");
+ }
+ else
+ {
+   // some other error interrupted the read
+  printf("File error\n");
+ }
 
-  char *buffer = NULL;
-  int read;
-  unsigned int len;
+ printf("Out of loop");
 
-  read = getline(&buffer, &len, stdin);
-  if (-1 != read)
-      puts(buffer);
-  else
-      printf("No line read...\n");
-  printf("Size read: %d\n Len: %d\n", read, len);
-  free(buffer);
-  return 0;
-
-  // while (prompt_and_read("\tNext number please.\n", &value) == 0)
-  // {
-  //     sum += value;
-  //     num++;
-  // }
-
-  // if (num > 0)
-  // {
-  //     printf("You entered %d numbers\n", num);
-  //     printf("Your sum     is %f\n", sum);
-  //     printf("Your average is %f\n", sum / num);
-  // }
-
-  // return 0;
+ return 0;
 }
