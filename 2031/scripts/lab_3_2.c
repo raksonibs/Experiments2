a9154baca8fb11e5a6627d683c3aef5eb7878c5d
Oskar Niburski <oskarniburski@gmail.com>
2016-01-21 16:55:49 -0500

    Conflicting types error

---
+++ 2031/scripts/lab_3_2.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#define MAXSIZE 50
+
+int stack[MAXSIZE];
+int top = 0;
+void push(int i);
+
+int main()
+{
+  char input[1];
+  while(1)
+  {
+    scanf("%s", input);
+    int val = atoi(input);
+    if(input[0] == 'q')
+    {
+      break;
+    }
+    else if(input[0] == 'p')
+    {
+      printf("%10d\n", stack[top]);
+    }
+    else if(input[0] == '+')
+    {
+      int j = pop();
+      int k = pop();
+      push(j + k);
+    }
+    else if(input[0] == '-')
+    {
+      int j = pop();
+      int k = pop();
+      push(k - j);
+    }
+    else if(input[0] == '*')
+    {
+      int j = pop();
+      int k = pop();
+      push(k * j);
+    }
+    else if(input[0] == '/')
+    {
+      int j = pop();
+      int k = pop();
+      push(k / j);
+    }
+    else
+    {
+      push(val);
+    }
+  }
+  return 0; 
+}
+
+void push(int i)
+{
+  if(top > MAXSIZE)
+  {
+    printf("Stack is full.\n");
+    return;
+  }
+  else
+  {
+    top++;
+    stack[top] = i;
+    return;
+  } 
+}
+
+int pop()
+{
+  int i;
+  if(top <= 0)
+  {
+    printf("Stack is empty.\n");
+    return 0;
+  }
+  else
+  {
+    i = stack[top];
+    top--;
+  }
+  return i;
+}
