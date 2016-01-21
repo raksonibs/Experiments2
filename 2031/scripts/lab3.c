bb88422725993ac71f30b91196fed8af9092d399
Oskar Niburski <oskarniburski@gmail.com>
2016-01-20 23:49:06 -0500

    This doesnt work

--- 2031/scripts/lab3.c
+++ 2031/scripts/lab3.c
@@ -9,7 +9,7 @@ main() {
  FILE *stream;
  stream = fopen("input_3.txt" , "rb");
  char * number;
- int * stack[50];
+ char * stack[50];
  int count = 1;
  int last_num;
  int second_last_num;
@@ -23,61 +23,57 @@ main() {
 
   pch = strtok (buffer," ");
   printf("Buffer here: %s\n", pch);
-  printf("Last Val: %d\n", stack[count]);
-  
+  printf("Last Val: %s\n", stack[(strlen(stack)-1)]);
+
   if (strcmp(pch, "p") == 0) {
     printf("Printing\n");
-    printf("Last Val: %d\n", stack[count]);
+    printf("Last Val: %d\n", stack[(strlen(stack)-1)]);
   } else if (strcmp(pch, "q") == 0) {
     printf("Quiting\n");
     break;
   } else if (strcmp(pch, "+") == 0) {
     printf("Adding\n");
-    last_num = stack[count];
-    second_last_num = stack[second_count];
-    stack[count] = NULL;
-    stack[second_count] = NULL;
+    last_num = atoi(stack[(strlen(stack)-1)]);
+    second_last_num = atoi(stack[(strlen(stack)-2)]);
+    stack[(strlen(stack)-1)] = NULL;
+    stack[(strlen(stack)-2)] = NULL;
     result = last_num + second_last_num;
-    stack[second_count] = result;
-    second_count = second_count - 2;
-    count = count - 1;
+    stack[(strlen(stack)-2)] = result;        
   } else if (strcmp(pch, "-") == 0) {
     printf("Subtracting\n");
-    last_num = stack[count];
-    second_last_num = stack[second_count];
-    stack[count] = NULL;
-    stack[second_count] = NULL;
+    last_num = atoi(stack[(strlen(stack)-1)]);
+    second_last_num = atoi(stack[(strlen(stack)-2)]);
+    stack[(strlen(stack)-1)] = NULL;
+    stack[(strlen(stack)-2)] = NULL;
     result = last_num - second_last_num;
-    stack[second_count] = result;
-    second_count = second_count - 2;
-    count = count - 1;
+    stack[(strlen(stack)-2)] = result;        
   } else if (strcmp(pch, "/") == 0) {
     printf("Dividing\n");
-    last_num = stack[count];
-    second_last_num = stack[second_count];
-    stack[count] = NULL;
-    stack[second_count] = NULL;
+    last_num = atoi(stack[(strlen(stack)-1)]);
+    second_last_num = atoi(stack[(strlen(stack)-2)]);
+    stack[(strlen(stack)-1)] = NULL;
+    stack[(strlen(stack)-2)] = NULL;
     result = last_num / second_last_num;
-    stack[second_count] = result;
-    second_count = second_count - 2;
-    count = count - 1;
+    stack[(strlen(stack)-2)] = result;        
   } else if (strcmp(pch, "*") == 0) {
     printf("Multiplying\n");
-    last_num = stack[count];
-    second_last_num = stack[second_count];
-    stack[count] = NULL;
-    stack[second_count] = NULL;
+    last_num = atoi(stack[(strlen(stack)-1)]);
+    second_last_num = atoi(stack[(strlen(stack)-2)]);
+    stack[(strlen(stack)-1)] = NULL;
+    stack[(strlen(stack)-2)] = NULL;
     result = last_num * second_last_num;
-    stack[second_count] = result;
-    second_count = second_count - 2;
-    count = count - 1;
+    stack[(strlen(stack)-2)] = result;        
   } else {
     // number
     printf("Numbering\n");
-    next_num = atoi(pch);
-    stack[count] = next_num;
-    count = count + 1;
-    second_count = second_count + 1;
+    next_num = pch;
+    if (strlen(stack) == 0) {
+      stack[(strlen(stack))] = next_num;
+    } else {
+
+      stack[(strlen(stack)-1)] = next_num;
+    }
+    printf("Last val after addition %s\n", stack[(strlen(stack))]);
   }
 
   printf("next line \n\n");
