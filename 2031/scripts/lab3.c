6f9c2fde549ffa880c93f297644ad3c2af413da5
Oskar Niburski <oskarniburski@gmail.com>
2016-01-21 10:20:29 -0500

    Trivial

--- 2031/scripts/lab3.c
+++ 2031/scripts/lab3.c
@@ -31,52 +31,53 @@ main() {
  printf("Stack is %s\n", stack);
 
  // problem with current code is with number like -7 or 35, string doesn't preserve it. need commas then break it up!
+ // alternate solution is to store with commas, and break with commas
 
  while (fgets(buffer, sizeof buffer, stream) != NULL)
- {
-  int i = 0;
-  len = strlen(stack);
-  printf("Length of stack at start %d\n", len);
-  printf("Stack before addition %s\n", stack);
-  pch = strtok (buffer,"\n");
-  printf("Buffer here: %s\n", pch);
-  printf("Length - 1 calc: %d\n", len - 1);
-  printf("Last Val before going into anything: %c\n", stack[len - 1]);
-
-  if (strcmp(pch, "p") == 0) {
-    printf("Printing\n");
-    printf("Last Val in print loop: %c\n", stack[len - 1]);
-  } else if (strcmp(pch, "q") == 0) {
-    printf("Quiting\n");
-    break;
-  } else if (strcmp(pch, "+") == 0) {
-    printf("Addinggg\n");
+  {
+    int i = 0;
     len = strlen(stack);
     printf("Length of stack at start %d\n", len);
-    printf("Last Val before addition: %c\n", stack[len - 1]);
-    printf("Second Last Val before addition: %c\n", stack[len - 2]);
-    printf("Trying to atoi\n");
-    last_num = stack[len - 1] - '0';
-    second_last_num = stack[len - 2] - '0';
-    printf("Successful atoi\n");
-    printf("Last Val before atoi addition: %d\n", last_num);
-    printf("Second Last Val before atoi addition: %d\n", second_last_num);
-    result = last_num + second_last_num;
-    printf("Addition result: %d\n", result);
-    sprintf(str, "%d", result);
-    printf("Reassigning last two values to nill and setting result to final value: %s\n", str);
-    stack[len - 1] = '\0';
-    len = strlen(stack);
-    printf("Length of stack at after first subtract %d\n", len);
+    printf("Stack before addition %s\n", stack);
+    pch = strtok (buffer,"\n");
+    printf("Buffer here: %s\n", pch);
+    printf("Length - 1 calc: %d\n", len - 1);
+    printf("Last Val before going into anything: %c\n", stack[len - 1]);
 
-    printf("stack is now: %s\n", stack);
-    printf("Reassigning the next nill\n");
-    stack[len - 1] = '\0';
-    printf("stack is now this: %s\n", stack);
-    len = strlen(stack);
-    printf("Length of stack at after second subtract %d\n", len);
-    strcat(stack, str);
-    printf("Stack after actual math addition result %s\n", stack);
+    if (strcmp(pch, "p") == 0) {
+      printf("Printing\n");
+      printf("Last Val in print loop: %c\n", stack[len - 1]);
+    } else if (strcmp(pch, "q") == 0) {
+      printf("Quiting\n");
+      break;
+    } else if (strcmp(pch, "+") == 0) {
+      printf("Addinggg\n");
+      len = strlen(stack);
+      printf("Length of stack at start %d\n", len);
+      printf("Last Val before addition: %c\n", stack[len - 1]);
+      printf("Second Last Val before addition: %c\n", stack[len - 2]);
+      printf("Trying to atoi\n");
+      last_num = stack[len - 1] - '0';
+      second_last_num = stack[len - 2] - '0';
+      printf("Successful atoi\n");
+      printf("Last Val before atoi addition: %d\n", last_num);
+      printf("Second Last Val before atoi addition: %d\n", second_last_num);
+      result = last_num + second_last_num;
+      printf("Addition result: %d\n", result);
+      sprintf(str, "%d", result);
+      printf("Reassigning last two values to nill and setting result to final value: %s\n", str);
+      stack[len - 1] = '\0';
+      len = strlen(stack);
+      printf("Length of stack at after first subtract %d\n", len);
+
+      printf("stack is now: %s\n", stack);
+      printf("Reassigning the next nill\n");
+      stack[len - 1] = '\0';
+      printf("stack is now this: %s\n", stack);
+      len = strlen(stack);
+      printf("Length of stack at after second subtract %d\n", len);
+      strcat(stack, str);
+      printf("Stack after actual math addition result %s\n", stack);
     } else if (strcmp(pch, "-") == 0) {
       printf("Subtracting\n");
       len = strlen(stack);
@@ -161,34 +162,35 @@ main() {
       printf("Length of stack at after second subtract %d\n", len);
       strcat(stack, str);
       printf("Stack after actual math multiplying result %s\n", stack);
-  } else {
-    // number
-    printf("Numbering\n");
-    next_num = pch;
-    printf("Numbering this number %s\n", pch);
-    len = strlen(stack);
-    printf("Length before add %d\n", len);
-    printf("Length - 1 calc: %d\n", len - 1);
-    // char *str = stradd(stack, pch);
-    // printf("String copied output %s\n", str);
-    strcat(stack, pch);
-    len = strlen(stack);
-    printf("Length after add %d\n", len);
-    printf("Length - 1 calc: %d\n", len - 1);
-    printf("Stack after addition %s\n", stack);
-    printf("Last val after addition %c\n", stack[len - 1]);
-  }
+    } else {
+      // number
+      printf("Numbering\n");
+      next_num = pch;
+      printf("Numbering this number %s\n", pch);
+      len = strlen(stack);
+      printf("Length before add %d\n", len);
+      printf("Length - 1 calc: %d\n", len - 1);
+      // char *str = stradd(stack, pch);
+      // printf("String copied output %s\n", str);
+      strcat(stack, pch);
+      len = strlen(stack);
+      printf("Length after add %d\n", len);
+      printf("Length - 1 calc: %d\n", len - 1);
+      printf("Stack after addition %s\n", stack);
+      printf("Last val after addition %c\n", stack[len - 1]);
+    }
 
-  printf("next line \n\n");
+    printf("next line \n\n");
+    
+    }
+    if (feof(stream))
+    {     
+    }
+    else
+    {   
+      printf("File error\n");
+    }
   
-  }
-  if (feof(stream))
-  {     
-  }
-  else
-  {   
-    printf("File error\n");
-  }
   printf("Done!");
 
   return 0;
--- 2031/scripts/lab3_1.c
+++ 2031/scripts/lab3_1.c
@@ -15,7 +15,7 @@ main() {
  FILE *stream;
  stream = fopen("input_3.txt" , "rb");
  char * number;
- int stack[50];
+ int stack[50] = {1,2,3,4};
  int count = 1;
  int last_num;
  int second_last_num;
@@ -28,55 +28,59 @@ main() {
  char string_val;
  int len;
  char str[15];
- printf("Stack is %s\n", stack);
+ len = sizeof stack;
+ for (int i = 0; i < len; i++) {
+  printf("%d\n", i);
+ }
+ printf("Stack is %d\n", stack);
 
- while (fgets(buffer, sizeof buffer, stream) != NULL)
- {
-  pch = strtok (buffer,"\n");
+ // while (fgets(buffer, sizeof buffer, stream) != NULL)
+ // {
+ //  pch = strtok (buffer,"\n");
   
-  if (strcmp(pch, "p") == 0) {
-    printf("Printing\n");
-    printf("Last Val in print loop: %d\n", stack[len - 1]);
-  } else if (strcmp(pch, "q") == 0) {
-    printf("Quiting\n");
-    break;
-  } else if (strcmp(pch, "+") == 0) {
-    printf("Addinggg\n");
+ //  if (strcmp(pch, "p") == 0) {
+ //    printf("Printing\n");
+ //    printf("Last Val in print loop: %d\n", stack[len - 1]);
+ //  } else if (strcmp(pch, "q") == 0) {
+ //    printf("Quiting\n");
+ //    break;
+ //  } else if (strcmp(pch, "+") == 0) {
+ //    printf("Addinggg\n");
     
-    } else if (strcmp(pch, "-") == 0) {
-      printf("Subtracting\n");
+ //    } else if (strcmp(pch, "-") == 0) {
+ //      printf("Subtracting\n");
       
-    } else if (strcmp(pch, "/") == 0) {
-      printf("Dividing\n");
+ //    } else if (strcmp(pch, "/") == 0) {
+ //      printf("Dividing\n");
       
-    } else if (strcmp(pch, "*") == 0) {
-      printf("Multiplying\n");
-    } else {
-    // number
-      printf("Numbering\n");
-      next_num = atoi(pch);
-      printf("Numbering this number %s\n", pch);
-      len = strlen(stack);
-      printf("Length before add %d\n", len);
-      printf("Length - 1 calc: %d\n", len - 1);
-      stack[len] = next_num;
-      len = strlen(stack);
-      printf("Length after add %d\n", len);
-      printf("Length - 1 calc: %d\n", len - 1);
-      printf("Stack after addition %d\n", stack);
-      printf("Last val after addition %d\n", stack[len - 1]);
-    }
+ //    } else if (strcmp(pch, "*") == 0) {
+ //      printf("Multiplying\n");
+ //    } else {
+ //    // number
+ //      printf("Numbering\n");
+ //      next_num = atoi(pch);
+ //      printf("Numbering this number %s\n", pch);
+ //      len = strlen(stack);
+ //      printf("Length before add %d\n", len);
+ //      printf("Length - 1 calc: %d\n", len - 1);
+ //      stack[len] = next_num;
+ //      len = strlen(stack);
+ //      printf("Length after add %d\n", len);
+ //      printf("Length - 1 calc: %d\n", len - 1);
+ //      printf("Stack after addition %d\n", stack);
+ //      printf("Last val after addition %d\n", stack[len - 1]);
+ //    }
 
-    printf("next line \n\n");
+ //    printf("next line \n\n");
   
-  }
-  if (feof(stream))
-  {     
-  }
-  else
-  {   
-    printf("File error\n");
-  }
+ //  }
+ //  if (feof(stream))
+ //  {     
+ //  }
+ //  else
+ //  {   
+ //    printf("File error\n");
+ //  }
   printf("Done!");
 
   return 0;
