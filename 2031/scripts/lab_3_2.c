dbc3e934c9c89cc2d261d50fc60ba577a9913dcd
Oskar Niburski <oskarniburski@gmail.com>
2016-01-21 18:28:35 -0500

    Formating

--- 2031/scripts/lab_3_2.c
+++ 2031/scripts/lab_3_2.c
@@ -5,78 +5,55 @@ int stack[MAXSIZE];
 int top = 0;
 void push(int i);
 
-int main()
-{
+int main() {
   char input[1];
-  while(1)
-  {
+  while(1) {
     scanf("%s", input);
     int val = atoi(input);
-    if(input[0] == 'q')
-    {
+    if (input[0] == 'q') {
       break;
-    }
-    else if(input[0] == 'p')
-    {
+    } else if (input[0] == 'p') {
       printf("%10d\n", stack[top]);
-    }
-    else if(input[0] == '+')
-    {
+    } else if(input[0] == '+') {
       int j = pop();
       int k = pop();
       push(j + k);
-    }
-    else if(input[0] == '-')
-    {
+    } else if (input[0] == '-') {
       int j = pop();
       int k = pop();
       push(k - j);
-    }
-    else if(input[0] == '*')
-    {
+    } else if(input[0] == '*') {
       int j = pop();
       int k = pop();
       push(k * j);
-    }
-    else if(input[0] == '/')
-    {
+    } else if(input[0] == '/') {
       int j = pop();
       int k = pop();
       push(k / j);
-    }
-    else
-    {
+    } else {
       push(val);
     }
   }
   return 0; 
 }
 
-void push(int i)
-{
-  if(top > MAXSIZE)
-  {
-    printf("Stack is full.\n");
+void push(int i) {
+  if (top > MAXSIZE) {
+    printf("Stack is already full!\n");
     return;
-  }
-  else
-  {
+  } else {
     top++;
     stack[top] = i;
     return;
   } 
 }
 
-int pop()
-{
+int pop() {
   int i;
-  if(top <= 0)
-  {
-    printf("Stack is empty.\n");
+  if(top <= 0) {
+    printf("Stack is empty!\n");
     return 0;
-  }
-  else
-  {
+  } else {
     i = stack[top];
     top--;
   }
