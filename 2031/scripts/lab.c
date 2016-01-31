23f175404c64955ad0b4e8a3f5cc3553df95ba3e
Oskar Niburski <oskarniburski@gmail.com>
2016-01-31 13:57:42 -0500

    Redo of lab 1

---
+++ 2031/scripts/lab.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void main() {
+
+  int string, sum = 0;
+
+  while((scanf("%s", &string)) ) {
+    if (i < 0) {
+      break;
+    }
+
+    sum += i;
+  }
+
+  printf("%10d\n", sum);
+
+}
+
