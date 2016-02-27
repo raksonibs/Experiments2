f00ebc4d47e6da75f730145cab06b7e65aba266a
Oskar Niburski <oskarniburski@gmail.com>
2016-02-27 17:20:14 -0500

    Working, but logic off

--- 2031/scripts/L6.c
+++ 2031/scripts/L6.c
@@ -10,6 +10,7 @@ int main(void) {
   int numGenerations;
   int i = 0;
   int j = 0;
+  int z = 0;
 
   printf("Put the number of rows and columns\n");
   scanf("%d %d", &numRows, &numCols);
@@ -24,6 +25,8 @@ int main(void) {
   int currentRow = 0;
   int currentCol = 0;
   int currentString;
+
+  int numNeighbours = 0;
 
   // then receives r lines with c number of integers
   // how would this come in during the test?
@@ -50,16 +53,117 @@ int main(void) {
 
   printf("Done Scanning, now outputting arr\n");
 
+  // rule block when iterating
 
+  for (z=0; z < numGenerations; z++) {
 
-  for (i=0; i<numRows;i++) {
+    for (i=0; i<numRows;i++) {
     
-    for (j=0; j<numCols;j++) {
-      printf("%d ", arr[i][j]);
+      for (j=0; j<numCols;j++) {
+        if (arr[i][j] == 1) {
+          // if 1, check neighbours (horizontal, vertical, diagonal)
+          // if two or three ones beside it, it is survives
+          // if more than two or three ones around it, it dies
+          // bottom center
+          if (arr[i+1][j] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // bottom right
+          if (arr[i+1][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+          // bottom left
+          if (arr[i+1][j-1] == 1) {
+            numNeighbours += 1; 
+          }
+          //right
+          if (arr[i][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+          //left
+          if (arr[i+1][j] == 1) {
+            numNeighbours += 1; 
+          }
+          // top center
+          if (arr[i-1][j] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // top left
+          if (arr[i-1][j-1] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // top right
+          if (arr[i-1][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+
+          if (numNeighbours <= 3) {
+            // survives
+          } else {
+            // dies overpopulation
+            arr[i][j] = 0;
+          }
+          numNeighbours=0;
+
+        } else {
+          // check if exactly three neighbours
+          if (arr[i+1][j] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // bottom right
+          if (arr[i+1][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+          // bottom left
+          if (arr[i+1][j-1] == 1) {
+            numNeighbours += 1; 
+          }
+          //right
+          if (arr[i][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+          //left
+          if (arr[i+1][j] == 1) {
+            numNeighbours += 1; 
+          }
+          // top center
+          if (arr[i-1][j] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // top left
+          if (arr[i-1][j-1] == 1) {
+            numNeighbours += 1; 
+          }
+
+          // top right
+          if (arr[i-1][j+1] == 1) {
+            numNeighbours += 1; 
+          }
+
+          if (numNeighbours == 3) {
+            // recreates
+            arr[i][j] = 1;
+          } else {
+            // nothing
+          }
+
+          numNeighbours=0;
+        }
+        printf("%d ", arr[i][j]);
+      }
+
+      printf("\n");
     }
 
-    printf("\n");
+    printf("-----\n");
   }
+
+  
 
    
   
