43a6251ff5e63fa212a2d12077fad1ee46ee7965
Oskar Niburski <oskarniburski@gmail.com>
2016-02-29 17:17:36 -0500

    Match 1

--- 2031/scripts/winter3.c
+++ 2031/scripts/winter3.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-
+// 56257278
+//0,2,3,4,5,6
 int main() {
   char input[2000];
 
@@ -10,16 +11,26 @@ int main() {
   int j = 0;
 
   char output[4000];
+  int match = 0;
 
   fgets(input, sizeof(input), stdin);
 
   for (i=0; i < strlen(input)-1; i++) {
+    match = 0;
     // printf("Current num %c\n,", input[i]);
-    for (j=i; j < strlen(input)-1; j++) {
+    for (j=0; j < strlen(input)-1; j++) {
       // printf("Comparing num %c\n,", input[j]);
       // this comparison with other method doesn't work, because these guys are all cgoing to return a position
-      if (input[i] == input[j]) {
-        printf("%d,", j);
+      if (input[i] == input[j] && i != j) {
+        match = 1;
+      }
+    }
+
+    if (match == 1) {
+      if (i == strlen(input)-3) {        
+        printf("%d", i);
+      } else {
+        printf("%d,", i);
       }
     }
 
