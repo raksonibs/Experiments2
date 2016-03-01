673188b965ffea91df06afdb491a350367a46ccb
Oskar Niburski <oskarniburski@gmail.com>
2016-03-01 17:34:07 -0500

    Match

--- 2031/scripts/ex3.c
+++ 2031/scripts/ex3.c
@@ -10,10 +10,28 @@ int main(int argc, char const *argv[])
 
   char line[2000];
 
+  char * pch;
+  int j = 0;
+
   while( (c = getchar()) != EOF) {
     if (c == '\n') {
+      // if match
+      printf("line is %s\n", line);
+      // proper match would loop through each char of line and see if works
+      //  would break match up into how many words
+      for (j=0; j < 2000 ;j++) {
+        if (line[j] == 'c' && line[j + 1] == 'a' && line[j + 2] == 't') {
+          printf("match: %s", line);
+          break;
+        }
+      }
+      if (strcmp(line, "cat") == 0) {
+        printf("match");
+      }
+
       line[i] = '\0';
-      break;
+      i = -1;
+
     } else {
       printf("%c\n", c);
       line[i] = c;
