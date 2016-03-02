1ef3dfee9223b775bc3aa1ce60a234723907ab68
Oskar Niburski <oskarniburski@gmail.com>
2016-03-02 09:25:51 -0500

    Trivial

--- 2031/scripts/winter8.c
+++ 2031/scripts/winter8.c
@@ -24,10 +24,28 @@ main() {
 
   printf("test\n");
 
+  int internal_count = 0;
+
   while(fgets(line, sizeof(line), file) != NULL) {
+    // wants to reallocate memory after five, would just use realloc on the table!
     if (i > 0) {
-      
+      // can i use scanf or some strcpy here to grab them all in one go?
+      pch = strtok(line, " ");
+      while (pch != NULL) {
+        if (internal_count == 0) {
+          strcpy(students[i].first_name, pch);          
+        }
+        internal_count++;
+        pch = strtok(NULL, " ");
+      } 
+
+      internal_count = 0;     
     }
+
     i++;
+  }
+  int k=0;
+  for (k=0; k < 12;k++) {
+    printf("%s\n", students[k].first_name);
   }
 }
