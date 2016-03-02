35c7483c7087cb0ee0ff1c8788e9c9333d679f3f
Oskar Niburski <oskarniburski@gmail.com>
2016-03-01 23:03:38 -0500

    Student program

--- 2031/scripts/winter4.c
+++ 2031/scripts/winter4.c
@@ -46,6 +46,12 @@ main() {
         } else if (internal_count == 3) {
           test_score = atof(pch);
           students[i].test_1 = test_score;
+        } else if (internal_count == 4) {
+          test_score = atof(pch);
+          students[i].test_2 = test_score;
+        } else if (internal_count == 5) {
+          test_score = atof(pch);
+          students[i].test_3 = test_score;
         }
 
         internal_count++;
@@ -61,4 +67,57 @@ main() {
 
 
   }
+
+  printf("please output a command!\n");
+  char input[2000];
+
+  char c;
+  int j =0;
+  int test_number = 0;
+  char first_arg[4];
+  char second_arg[200];
+  scanf("%s %s", first_arg, second_arg);
+  printf("Searching now!\n");
+  if (first_arg[0] == 'T') {
+    // turn char into c
+    test_number = first_arg[1] - '0';
+
+    for (j=0; j < sizeof(students); j++) {
+      if (strcmp(students[j].last_name, second_arg) == 0) {
+        if (test_number == 1) {
+          printf("First test score %f\n", students[j].test_1);
+        } else if (test_number == 2) {
+          printf("First test score %f\n", students[j].test_2);
+        } else if (test_number == 3) {
+          printf("First test score %f\n", students[j].test_3);
+        }
+      }
+    }
+  } else if (first_arg[0] == 'A') {
+    // average for student, pretty easy find last name get all scores
+  } else {
+    // class average for certain test. Pretty easy, loop through all and get test
+  }
+
+  // while ( (c = getchar()) != EOF ) {
+  //   if (c == '\n') {
+  //     printf("Full command: %s", input);
+
+  //     pch = strtok(input, " ");
+
+  //     while (pch != NULL) {
+  //       if (pch.getchar(0) == "T") {
+  //         test_number = atoi(pch.getchar(1));
+
+
+  //       }
+  //     }
+  //     j = 0;
+  //   }
+  //   printf("%c\n", c);
+  //   input[j] = c;
+  //   j++;
+
+  // }
+  
 }
