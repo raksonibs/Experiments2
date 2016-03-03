da45726091fc122a6041f02c6db7f137f6058565
Oskar Niburski <oskarniburski@gmail.com>
2016-03-02 23:16:39 -0500

    Winter14

---
+++ 2031/scripts/winter14.c
@@ -0,0 +1,42 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+main() {
+  char input [20000];
+
+  fgets(input, sizeof(input), stdin);
+
+  if (input[0] == '.') {
+    printf("Period first\n");
+  }
+
+  int count=0;
+  int wordLength= 0;
+  int words = 0;
+  int totalWords = 0;
+  int i = 0;
+  for (i=0; i <strlen(input) ;i++) {
+    if (input[i] == '.') {
+      totalWords += wordLength;
+      wordLength = 0;
+      words++;
+      break;
+    }
+
+    
+    if (input[i] == ' ') {
+      totalWords += wordLength;
+      wordLength = 0;
+      words++;
+    } else {
+      wordLength++;
+      printf("Current wordlength: %d\n", wordLength);
+    }
+
+  }
+
+  printf("WOrds: %d\n", words);
+  printf("Total WOrds: %d\n", totalWords);
+  printf("AvG: %f\n", ((double)totalWords)/words);
+}
