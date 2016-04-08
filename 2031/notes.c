2238019d8740b4ef48372b142dc17993ef837179
Oskar Niburski <oskarniburski@gmail.com>
2016-04-07 23:51:06 -0400

    First note

---
+++ 2031/notes.c
@@ -0,0 +1,16 @@
+// 1978 - deniis ritchie brian kernighn
+// ansi completed standard for C approved in 1989 known as c89
+// - c99 standard
+// almost low level, small permissive
+// efficent
+// ideas/specs, design, debugging coding, program, testing
+// testing show presnce of faults, not their absence
+// syntax, runtime, logic
+// runtime errors: violation of rules during execution
+// logical -> not detected by compiler may not produce error
+// no garbage collection, no classes, no exception, no type strings
+// 4 basic types: char, int, float, double
+// modifiers -> signed (unsigned) int long int, 
+// char -> one byte // reg bools. 
+// i/o -> stderr, stdin, stdout  
+//  %d int, %s string, %c char, %f float, %if double precision
