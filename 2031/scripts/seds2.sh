ff76ef816e1a53fa3f451e7b8bf1731c6d89883e
Oskar Niburski <oskarniburski@gmail.com>
2016-04-12 10:47:54 -0400

    Random notes

---
+++ 2031/scripts/seds2.sh
@@ -0,0 +1,14 @@
+# sed 's/Jon/Jonathan/' < emails
+# sed -n '/PA/p' < emails
+# sed -n '/Karen Evich/c\*** karen has retired****'
+# sed '1 i\Beginning of db'
+# sed -n '/NJ/a\**NEWjesrsy'
+# s/.*\([0-9]\{3\}-[0-9]\{3\}-[0-9]\{4\}\).*/\1/p
+
+# if [ ! -z "$2" ]; then
+#   echo "Two arguments"
+#   echo `expr $1 + $2`
+# else
+#   echo "Neews two"
+# fi
+
---
+++ 2031/scripts/this.c
@@ -0,0 +1,45 @@
+// // #include <stdio.h>
+// // #include <stdlib.h>
+// // #include <string.h>
+
+// // main() {
+// //   char name[200];
+// //   fgets(name, sizeof(name), stdin);
+// //   char * pch;
+
+// //   pch = strtok(name, " \n");
+
+// //   while (pch != NULL) {
+// //     printf("%s\n", pch);
+// //     pch = strtok(NULL, " \n");
+// //   }
+
+// // }
+
+// main() {
+//   FILE *fp;
+
+//   char line[200];
+//   fp = fopen("grades.txt", "r")
+//   struct student {
+//     char first_name[20];
+//     char last_name[20];
+//     char email[200];
+//     float test_1;
+//   };
+
+//   struct student *students;
+//   students = (struct student*)malloc(5*sizeof(struct student));
+//   char *pch;
+//   while(line, sizeof(line), fp) {
+
+//   }
+// }
+
+main() {
+  char input[2000];
+
+  char c;
+
+  while (c = getchar())
+}
