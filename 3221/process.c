f40c0df7a5e660a2cb897d28751a4539fcbf0f63
Oskar Niburski <oskarniburski@gmail.com>
2016-06-08 00:42:21 -0400

    Triv

--- 3221/process.c
+++ 3221/process.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main() {
   // for each data set, make a child process
@@ -12,24 +13,67 @@ int main() {
 
   int pid;
 
-  pid = fork();
-  // fork child process
+  char * arr[200];
 
-  if (pid < 0) {
-    // error occured
-    fprintf(stderr, "%s\n", "ouch");
-    return 1;
-  } else if (pid == 0) {
-    // child processs
-    printf("\n Hello I am the child process ");
-    printf("\n My pid is %d \n",getpid());
-    // execlp("/bin/ls", "ls", NULL);
-  } else {
-    // parent will wait for child to complete
-    wait(NULL);
-    printf("Child complete\n");
-    printf("\n Hello I am the parent process ");
-    printf("\n My actual pid is %d \n ",getpid());
+  char * pch;
+
+  char str[5000];
+
+  FILE * fp;
+  int count = 0;
+
+  // fgets(stdin, arr, sizeof(arr));
+
+  // printf("%s", arr);
+
+   // fp = fopen("file.txt" , "r");
+   // if(fp == NULL) 
+   // {
+   //    perror("Error opening file");
+   //    return(-1);
+   // }
+   // if( fgets (str, 60, fp)!=NULL ) 
+   // {
+   //    /* writing content to stdout */
+   //    puts(str);
+   // }
+   // fclose(fp);
+
+  // this will be reading from command line each data set
+  while (count < 3) {
+
+    pid = fork();
+    // fork child process
+
+    if (pid < 0) {
+      // error occured
+      fprintf(stderr, "%s\n", "ouch");
+      return 1;
+    } else if (pid == 0) {
+      // child processs
+      printf("\n Hello I am the child process ");
+      printf("\n My pid is %d \n",getpid());
+      fp = fopen("dataset1.txt" , "r");
+       if(fp == NULL) 
+       {
+          perror("Error opening file");
+          return(-1);
+       }
+       if( fgets (str, 60, fp)!=NULL ) 
+       {
+          /* writing content to stdout */
+          puts(str);
+       }
+       count++;
+       fclose(fp);
+      // execlp("/bin/ls", "ls", NULL);
+    } else {
+      // parent will wait for child to complete
+      wait(NULL);
+      printf("Child complete\n");
+      printf("\n Hello I am the parent process ");
+      printf("\n My actual pid is %d \n ",getpid());
+    }
   }
 
   return 0;
