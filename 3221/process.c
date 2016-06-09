4e53e79090413de8d1ec8fdc1673fa5d9887e476
Oskar Niburski <oskarniburski@gmail.com>
2016-06-09 00:45:52 -0400

    Read write pipes not working

--- 3221/process.c
+++ 3221/process.c
@@ -41,6 +41,7 @@ int main(int argc, char **argv) {
   float current;
   char buffer[1024];
   char * readFile;
+  int val = 0, len, i;
   // int fd[2];
   // pipe(fd);
 
@@ -61,61 +62,83 @@ int main(int argc, char **argv) {
       // child process
       close(fd[1]);
       int ret = dup2(fd[0],0);
-      if (ret < 0) perror("dup2");
+      // len = read(fd[0], &val, sizeof(val));
+      // if (len < 0) {
+      //     perror("Child: Failed to read data from pipe");
+      //     exit(EXIT_FAILURE);
+      // }
+      // else if (len == 0)
+      // {
+      //     // not an error, but certainly unexpected
+      //     fprintf(stderr, "Child: Read EOF from pipe");
+      // }
+      // else {
+        if (ret < 0) perror("dup2");
         // printf("\n Hello I am the child process ");
         // printf("\n My pid is %d \n",getpid());
-      readFile = argv[i];
+        readFile = argv[i];
+        // printf("Child(%d): Received %d\n", pid, val);
+        val *= 2;
+        // printf("Child(%d): Sending %d back\n", pid, val);
+        // if (write(fd[1], &val, sizeof(val)) < 0)
+        // {
+        //     perror("Child: Failed to write response value");
+        //     exit(EXIT_FAILURE);
+        // }
+        
         // printf("I am going to read %s\n", readFile);
-      fp = fopen(readFile, "r");
-      if(fp == NULL) 
-      {
-        perror("Error opening file");
-        return(-1);
-      }
-      if( fgets (str, sizeof(str), fp)!=NULL ) 
-      {
-        pch = strtok (str, " ");
-        while (pch != NULL)
+        fp = fopen(readFile, "r");
+        if (fp == NULL) 
         {
-              // printf ("%s\n",pch);
-          current = atof(pch);
+          perror("Error opening file");
+          return(-1);
+        }
+        if( fgets (str, sizeof(str), fp)!=NULL ) 
+        {
+          pch = strtok (str, " ");
+          while (pch != NULL)
+          {
+                // printf ("%s\n",pch);
+            current = atof(pch);
 
-          if (current > max) {
-            max = current;
+            if (current > max) {
+              max = current;
+            }
+
+            if (current < min) {
+              min = current;
+            } 
+
+            pch = strtok (NULL, " ");
           }
 
-          if (current < min) {
-            min = current;
-          } 
-
-          pch = strtok (NULL, " ");
         }
 
-      }
+        diff = min - max;
+        sum = min + max;
 
-      diff = min - max;
-      sum = min + max;
+        if (max > totalMax) {
+              // printf("Changing total Max: %f\n to %f\n", max, totalMax);
+          totalMax = max;
+              // printf("Changed total Max: %f\n\n", totalMax);
+        }
+        if (min < totalMin) {
+              // printf("Changing total Min: %f\n to %f\n", min, totalMin);
+          totalMin = min;
+              // printf("Changed total Min: %f\n\n", totalMin);
+        }
 
-      if (max > totalMax) {
-            // printf("Changing total Max: %f\n to %f\n", max, totalMax);
-        totalMax = max;
-            // printf("Changed total Max: %f\n\n", totalMax);
-      }
-      if (min < totalMin) {
-            // printf("Changing total Min: %f\n to %f\n", min, totalMin);
-        totalMin = min;
-            // printf("Changed total Min: %f\n\n", totalMin);
-      }
+        // dup2(fd[0], totalMin);
+        // dup2(fd[0], totalMax);
 
-      // dup2(fd[0], totalMin);
-      // dup2(fd[0], totalMax);
+        fclose(fp);
+            // printf("*****END CHILD PROCESS *******\n");
+        snprintf(buffer, sizeof(buffer), "Filename: %s, sum: %f, diff: %f, max: %f, min: %f", readFile, sum, diff, max, min);
+            // printf("Filename: %s, sum: %f, diff: %f, max: %f, min: %f\n", readFile, diff, sum, max, min);
 
-      fclose(fp);
-          // printf("*****END CHILD PROCESS *******\n");
-      snprintf(buffer, sizeof(buffer), "Filename: %s, sum: %f, diff: %f, max: %f, min: %f", readFile, sum, diff, max, min);
-          // printf("Filename: %s, sum: %f, diff: %f, max: %f, min: %f\n", readFile, diff, sum, max, min);
-
-      execl("/bin/echo", "echo", buffer, NULL);
+        execl("/bin/echo", "echo", buffer, NULL);
+      // }
+      
     } 
   }
 
