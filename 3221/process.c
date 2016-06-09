285f1258db9c3c6cd379d56d51afaa024578dcd5
Oskar Niburski <oskarniburski@gmail.com>
2016-06-08 22:49:29 -0400

    Need to pass to end of pipe?

--- 3221/process.c
+++ 3221/process.c
@@ -32,7 +32,7 @@ int main(int argc, char **argv) {
   FILE * fp;
   int count = 0;
   float max = 0;
-  float min = 0;
+  float min = 100000;
   float diff;
   float sum;
   float current;
@@ -41,7 +41,7 @@ int main(int argc, char **argv) {
   // int fd[2];
   // pipe(fd);
   float totalMax = 0;
-  float totalMin = 0;
+  float totalMin = 100000;
 
   count = argc - 1;
   int internalCount = 1;
@@ -56,51 +56,64 @@ int main(int argc, char **argv) {
     // printf("argv[%d]: %s\n", i, argv[i]);
     pid = fork();
     if (pid == 0) {
-        close(fd[1]);
-        int ret = dup2(fd[0],0);
-        if (ret < 0) perror("dup2");
+      // child process
+      close(fd[1]);
+      int ret = dup2(fd[0],0);
+      if (ret < 0) perror("dup2");
         // printf("\n Hello I am the child process ");
         // printf("\n My pid is %d \n",getpid());
-        readFile = argv[i];
+      readFile = argv[i];
         // printf("I am going to read %s\n", readFile);
-        fp = fopen(readFile, "r");
-         if(fp == NULL) 
-         {
-            perror("Error opening file");
-            return(-1);
-         }
-         if( fgets (str, sizeof(str), fp)!=NULL ) 
-         {
-            pch = strtok (str," ,.-");
-            while (pch != NULL)
-            {
+      fp = fopen(readFile, "r");
+      if(fp == NULL) 
+      {
+        perror("Error opening file");
+        return(-1);
+      }
+      if( fgets (str, sizeof(str), fp)!=NULL ) 
+      {
+        pch = strtok (str, " ");
+        while (pch != NULL)
+        {
               // printf ("%s\n",pch);
-              current = atof(pch);
-              if (current > max) {
-                max = current;
-              }
+          current = atof(pch);
 
-              if (current < min) {
-                min = current;
-              }
-              pch = strtok (NULL, " ,.-");
-            }
+          if (current > max) {
+            max = current;
+          }
 
-            diff = min - max;
-            sum = min + max;
+          if (current < min) {
+            min = current;
+          } 
 
-         }
-          fclose(fp);
+          pch = strtok (NULL, " ");
+        }
+
+      }
+
+      diff = min - max;
+      sum = min + max;
+
+      if (max > totalMax) {
+            // printf("Changing total Max: %f\n to %f\n", max, totalMax);
+        totalMax = max;
+            // printf("Changed total Max: %f\n\n", totalMax);
+      }
+      if (min < totalMin) {
+            // printf("Changing total Min: %f\n to %f\n", min, totalMin);
+        totalMin = min;
+            // printf("Changed total Min: %f\n\n", totalMin);
+      }
+
+      // dup2(fd[0], totalMin);
+      // dup2(fd[0], totalMax);
+
+      fclose(fp);
           // printf("*****END CHILD PROCESS *******\n");
-          snprintf(buffer, sizeof(buffer), "Filename: %s, sum: %f, diff: %f, max: %f, min: %f", readFile, diff, sum, max, min);
-          if (max > totalMax) {
-            totalMax = max;
-          }
-          if (min < totalMin) {
-            totalMin = min;
-          }
+      snprintf(buffer, sizeof(buffer), "Filename: %s, sum: %f, diff: %f, max: %f, min: %f", readFile, sum, diff, max, min);
           // printf("Filename: %s, sum: %f, diff: %f, max: %f, min: %f\n", readFile, diff, sum, max, min);
-          execl("/bin/echo", "echo", buffer, NULL);
+
+      execl("/bin/echo", "echo", buffer, NULL);
     } 
   }
 
@@ -110,7 +123,7 @@ int main(int argc, char **argv) {
   // wait for the more process to finish
   int status;
   waitpid(pid, &status, 0);
-  printf("MINIMUM=%f\tMAXIUMUM=%f", totalMax, totalMin);
+  printf("MINIMUM=%f\tMAXIUMUM=%f", totalMin, totalMax);
   // printf("Done!\n");
   return 0;
 }
