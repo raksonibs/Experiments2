5d9bb7d643de67522eba83f869b89eeea1508ad9
Oskar Niburski <oskarniburski@gmail.com>
2016-06-11 16:15:58 -0400

    Trivial

---
+++ 3221/ans.c
@@ -0,0 +1,4 @@
+// Filename: dataset2, sum: 100.984116, diff: -98.389618, max: 99.686867, min: 1.297250
+// Filename: dataset1, sum: 99.500092, diff: -99.390106, max: 99.445099, min: 0.054990
+// Filename: dataset3, sum: 99.915817, diff: -99.870956, max: 99.893387, min: 0.022430
+// MINIMUM=0.022430  MAXIUMUM=99.893387
